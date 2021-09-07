1685f1fcdff4a351874407b40893243de138d4b1
Raphael bourgeat <cosmosraph@gmail.com>
2021-09-07 17:20:21 +0200

    adding some tests

--- tests/vector_test.cpp
+++ tests/vector_test.cpp
@@ -6,7 +6,7 @@
 /*   By: rbourgea <rbourgea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 10:35:52 by rbourgea          #+#    #+#             */
-/*   Updated: 2021/09/07 15:43:17 by rbourgea         ###   ########.fr       */
+/*   Updated: 2021/09/07 17:20:02 by rbourgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # include <cstring>
 # include <iostream>
 
+# include <conio.h>
 
 # include "../vector.hpp"
 
@@ -96,7 +97,8 @@ int main(int argc, char** argv)
 	std::vector<int> stl_vector_int;
 	ft::vector<int> ft_vector_int;
 	checkVectorsDiff(stl_vector_int, ft_vector_int);
-	
+	getch();
+
 	std::cout << BOLD << "==============================" << RESET << std::endl;
 	std::cout << std::endl << "Default constructor pointer: " << std::endl;
 	std::vector<int> *stl_vector_int_p = new std::vector<int>;
@@ -151,7 +153,7 @@ int main(int argc, char** argv)
 	if (begin_stl == begin_ft)
 		std::cout << GREEN << std::endl << "=> begin() [OK]" << std::endl;
 	else
-		std::cout << RED << "begin() [ERROR]" << std::endl;
+		std::cout << RED << "=> begin() [ERROR]" << std::endl;
 	std::cout << "[STL] begin() = " << begin_stl << std::endl;
 	std::cout << "[FT]  begin() = " << begin_ft  << std::endl << std::endl;
 	
@@ -164,7 +166,7 @@ int main(int argc, char** argv)
 	if (const_begin_stl == const_begin_ft)
 		std::cout << GREEN << std::endl << "=> const begin() [OK]" << std::endl;
 	else
-		std::cout << RED << "const begin() [ERROR]" << std::endl;
+		std::cout << RED << "=> const begin() [ERROR]" << std::endl;
 	std::cout << "[STL] begin() = " << const_begin_stl << std::endl;
 	std::cout << "[FT]  begin() = " << const_begin_ft << std::endl << std::endl;
 
@@ -175,11 +177,151 @@ int main(int argc, char** argv)
 	if (end_stl == end_ft)
 		std::cout << GREEN << std::endl << "=> end() [OK]" << std::endl;
 	else
-		std::cout << RED << "end() [ERROR]" << std::endl;
+		std::cout << RED << "=> end() [ERROR]" << std::endl;
 	std::cout << "[STL] end() = " << end_stl << std::endl;
 	std::cout << "[FT]  end() = " << end_ft << std::endl << std::endl;
 
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "Const Iterator end(): " << std::endl;
+	std::vector<int>::const_iterator stl_const_end = stl_vector_range.end() - 1;
+	ft::vector<int>::const_iterator ft_const_end = ft_vector_range.end() - 1;
+	const int const_end_stl = *(stl_const_end);
+	const int const_end_ft = *(ft_const_end);
+	if (const_end_stl == const_end_ft)
+		std::cout << GREEN << std::endl << "=> const end() [OK]" << std::endl;
+	else
+		std::cout << RED << "=> const end() [ERROR]" << std::endl;
+	std::cout << "[STL] end() = " << const_end_stl << std::endl;
+	std::cout << "[FT]  end() = " << const_end_ft << std::endl << std::endl;
+	
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "Iterator rbegin(): " << std::endl;
+	int rbegin_stl = *(stl_vector_range.rbegin());
+	int rbegin_ft = *(ft_vector_range.rbegin());
+	if (rbegin_stl == rbegin_ft)
+		std::cout << GREEN << std::endl << "=> rbegin() [OK]" << std::endl;
+	else
+		std::cout << RED << "=> rbegin() [ERROR]" << std::endl;
+	std::cout << "[STL] rbegin() = " << rbegin_stl << std::endl;
+	std::cout << "[FT]  rbegin() = " << rbegin_ft << std::endl << std::endl;
+	
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "Const Iterator rbegin(): " << std::endl;
+	std::vector<int>::const_reverse_iterator stl_const_rbegin = stl_vector_range.rbegin();
+	ft::vector<int>::const_reverse_iterator ft_const_rbegin = ft_vector_range.rbegin();
+	const int const_rbegin_stl = *(stl_const_rbegin);
+	const int const_rbegin_ft = *(ft_const_rbegin);
+	if (const_rbegin_stl == const_rbegin_ft)
+		std::cout << GREEN << std::endl << "=> const rbegin() [OK]" << std::endl;
+	else
+		std::cout << RED << "=> const rbegin() [ERROR]" << std::endl;
+	std::cout << "[STL] rbegin() = " << const_rbegin_stl << std::endl;
+	std::cout << "[FT]  rbegin() = " << const_rbegin_ft << std::endl << std::endl;
 
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "Iterator rend(): " << std::endl;
+	int rend_stl = *(stl_vector_range.rend() - 1);
+	int rend_ft = *(ft_vector_range.rend() - 1);
+	if (rend_stl == rend_ft)
+		std::cout << GREEN << std::endl << "=> rend() [OK]" << std::endl;
+	else
+		std::cout << RED << "=> rend() [ERROR]" << std::endl;
+	std::cout << "[STL] rend() = " << rend_stl << std::endl;
+	std::cout << "[FT]  rend() = " << rend_ft << std::endl << std::endl;
+
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "Const Iterator rend(): " << std::endl;
+	std::vector<int>::const_reverse_iterator stl_const_rend = stl_vector_range.rend() - 1;
+	ft::vector<int>::const_reverse_iterator ft_const_rend = ft_vector_range.rend() - 1;
+	const int const_rend_stl = *(stl_const_rend);
+	const int const_rend_ft = *(ft_const_rend);
+	if (const_rend_stl == const_rend_ft)
+		std::cout << GREEN << std::endl << "=> const rend() [OK]" << std::endl;
+	else
+		std::cout << RED << "=> const rend() [ERROR]" << std::endl;
+	std::cout << "[STL] rend() = " << const_rend_stl << std::endl;
+	std::cout << "[FT]  rend() = " << const_rend_ft << std::endl << std::endl;
+
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "Size small: " << std::endl;
+	std::vector<int> stl_fill(21);
+	ft::vector<int> ft_fill(21);
+	checkVectorsDiff(stl_fill, ft_fill);
+	
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "Size big: " << std::endl;
+	std::vector<int> stl_fill2(100042);
+	ft::vector<int> ft_fill2(100042);
+	checkVectorsDiff(stl_fill2, ft_fill2);
+	
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "Resize +++: " << std::endl;
+	std::vector<int> stl_fill3(42);
+	ft::vector<int> ft_fill3(42);
+	stl_fill3.resize(84);
+	ft_fill3.resize(84);
+	checkVectorsDiff(stl_fill3, ft_fill3);
+	
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "Resize ---: " << std::endl;
+	std::vector<int> stl_fill4(42);
+	ft::vector<int> ft_fill4(42);
+	stl_fill4.resize(21);
+	ft_fill4.resize(21);
+	checkVectorsDiff(stl_fill4, ft_fill4);
+
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "Capacity small: " << std::endl;
+	std::vector<int> stl_fill5(42);
+	ft::vector<int> ft_fill5(42);
+	stl_fill5.push_back(21);
+	ft_fill5.push_back(21);
+	stl_fill5.push_back(84);
+	ft_fill5.push_back(84);
+	checkVectorsDiff(stl_fill5, ft_fill5);
+
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "Capacity big: " << std::endl;
+	std::vector<int> stl_fill6(42000);
+	ft::vector<int> ft_fill6(42000);
+	checkVectorsDiff(stl_fill6, ft_fill6);
+	
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "Reserve: " << std::endl;
+	std::vector<int> stl_reserve;
+	ft::vector<int> ft_reserve;
+	stl_reserve.reserve(42);
+	ft_reserve.reserve(42);
+	checkVectorsDiff(stl_reserve, ft_reserve);
+
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "Operator[]: " << std::endl;
+	if (stl_vector_range[0] == ft_vector_range[0])
+		std::cout << GREEN << std::endl << "=> operator[0] [OK]" << std::endl;
+	else
+		std::cout << RED << "=> operator[0] [ERROR]" << std::endl;
+	std::cout << "[STL] operator[0] = " << stl_vector_range[0] << std::endl;
+	std::cout << "[FT]  operator[0] = " << ft_vector_range[0] << std::endl << std::endl;
+
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "at(4): " << std::endl;
+	if (stl_vector_range.at(4) == ft_vector_range.at(4))
+		std::cout << GREEN << std::endl << "=> at(4) [OK]" << std::endl;
+	else
+		std::cout << RED << "=> a(4) [ERROR]" << std::endl;
+	std::cout << "[STL] at(4) = " << stl_vector_range.at(4) << std::endl;
+	std::cout << "[FT]  at(4) = " << ft_vector_range.at(4) << std::endl << std::endl;
+	
+	std::cout << BOLD << "==============================" << RESET << std::endl;
+	std::cout << std::endl << "const at(4): " << std::endl;
+	const int stl_at_const = stl_vector_range.at(4);
+	const int ft_at_const = ft_vector_range.at(4);
+	if (stl_at_const == ft_at_const)
+		std::cout << GREEN << std::endl << "=> const at(4) [OK]" << std::endl;
+	else
+		std::cout << RED << "=> const a(4) [ERROR]" << std::endl;
+	std::cout << "[STL] const at(4) = " << stl_at_const << std::endl;
+	std::cout << "[FT]  const at(4) = " << ft_at_const << std::endl << std::endl;
 
 
 
